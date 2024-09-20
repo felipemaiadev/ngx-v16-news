f772b0e86fa9cb7c29963c36f28d3754f7773e09
felipemaia <felipe.maia.rj@gmai.com>
2024-09-20 17:04:15 -0300

    add component to demonstrate computed signals

---
+++ src/app/dinamic-list/dinamic-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dinamic-list',
+  templateUrl: './dinamic-list.component.html',
+  styleUrls: ['./dinamic-list.component.css']
+})
+export class DinamicListComponent {
+
+}
